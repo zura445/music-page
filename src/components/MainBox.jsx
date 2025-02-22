faf3756caa9f2023028dffa27dae7cb020303b13
zura tetrashvili <zuratetra@gmail.com>
2025-02-22 16:40:22 +0400

    add mainbox images responsive

--- src/components/MainBox.jsx
+++ src/components/MainBox.jsx
@@ -2,20 +2,25 @@ import React from "react";
 
 function MainBox() {
   return (
-    <>
-      <div className="">
-        <div className="mx-6 lg:ml-[165px] md:mr-0 flex flex justify-center">
-          <img
-            className="w-[208px] mt-[68px]"
-            src="/phone.png"
-            alt="phone image"
-          />
-        </div>
-        <div className="mx-6 lg:ml-[165px] md:mr-0 flex flex justify-center">
-          <img src="/back-logo-small.png" alt="" />
-        </div>
+    <div className="flex justify-center flex-col items-center">
+      <div className="w-[208px] md:w-[270px] lg:w-[312px]">
+        <img
+          className="w-full mt-[68px]"
+          src="/phone-medium.png"
+          alt="phone image"
+        />
       </div>
-    </>
+      <div className="mt-4 md:hidden">
+        <img
+          className="w-full"
+          src="/back-logo-small.png"
+          alt="background image"
+        />
+      </div>
+      <div className="mt-4 hidden md:block w-full lg:max-w-[1110px] md:max-w-[696px]">
+        <img className="w-full" src="/back-logo.png" alt="background image" />
+      </div>
+    </div>
   );
 }
 
